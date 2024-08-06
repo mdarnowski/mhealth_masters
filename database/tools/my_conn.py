8d6855a963880efbba4c2b78a5bd5af122d578a7
mdarnowski <s18322@pjwstk.edu.pl>
2024-08-06 22:00:01 +0200

    optuna hyper-parametrization

--- database/tools/my_conn.py
+++ database/tools/my_conn.py
@@ -1,16 +1,26 @@
 import os
 from contextlib import contextmanager
 
+import numpy as np
+import psycopg2
 from dotenv import load_dotenv
+from psycopg2.extensions import AsIs, register_adapter
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+
+def adapt_numpy_int32(numpy_int32):
+    return AsIs(int(numpy_int32))
+
+
+register_adapter(np.int32, adapt_numpy_int32)
 
 load_dotenv()
 database_uri = os.getenv("DATABASE_URI")
 if database_uri is None:
     raise ValueError("DATABASE_URI not found in environment variables")
 
-db_engine = create_engine(database_uri)
+db_engine = create_engine(database_uri, pool_size=-1, max_overflow=-1)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 
--- study/a_rec.py
+++ study/a_rec.py
@@ -1,10 +1,10 @@
 import math
-import random
 
 import tensorflow as tf
 from keras import Input, Sequential
-from keras.src.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.src.callbacks import EarlyStopping
 from keras.src.layers import Dense
+from keras.src.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
 from data_layer import activity_data as act
@@ -15,7 +15,6 @@ from database.tools import my_conn
 
 def _split_dataset(test_size=0.1, val_size=0.1, seed=42):
     ids, labels = ses.get_session_label_ids()
-
     train_val_ids, test_ids, train_val_labels, _ = train_test_split(
         ids, labels, test_size=test_size, stratify=labels, random_state=seed
     )
@@ -26,7 +25,8 @@ def _split_dataset(test_size=0.1, val_size=0.1, seed=42):
         stratify=train_val_labels,
         random_state=seed,
     )
-    return train_ids, val_ids, test_ids
+    label_dict = dict(zip(ids, labels))
+    return train_ids, val_ids, test_ids, label_dict
 
 
 class ActivityRecognitionModel(Sequential):
@@ -35,10 +35,7 @@ class ActivityRecognitionModel(Sequential):
         segment_size,
         batch_size,
         n_shifts=1,
-        incl_acc=True,
-        incl_gyro=True,
-        incl_mag=True,
-        incl_ecg=True,
+        l_rate=0.001,
         *args,
         **kwargs,
     ):
@@ -47,28 +44,23 @@ class ActivityRecognitionModel(Sequential):
         self.segment_size = segment_size
         self.batch_size = batch_size
         self.n_shifts = n_shifts
-        self.incl_acc = incl_acc
-        self.incl_gyro = incl_gyro
-        self.incl_mag = incl_mag
-        self.incl_ecg = incl_ecg
-        self.n_features = self._count_features()
-        self.train_ids, self.val_ids, self.test_ids = _split_dataset()
+        self.n_features = len(sen.get_sensor_columns())
+
+        self.train_ids, self.val_ids, self.test_ids, self.label_dict = _split_dataset()
         with my_conn.get_db_session() as db:
             self.n_classes = act.count_activity(db)
+
+        self.l_rate = l_rate
 
     def train_model(self):
         train, train_steps = self._create_dataset(self.train_ids)
         val, val_steps = self._create_dataset(self.val_ids)
 
         early_stopping = EarlyStopping(
-            monitor="val_accuracy",
+            monitor="val_loss",
             patience=2,
             restore_best_weights=True,
             min_delta=0.01,
-        )
-
-        reduce_lr = ReduceLROnPlateau(
-            monitor="val_loss", factor=0.3, patience=1, min_lr=0.0001, cooldown=0
         )
 
         history = self.fit(
@@ -76,7 +68,7 @@ class ActivityRecognitionModel(Sequential):
             steps_per_epoch=train_steps,
             validation_data=val,
             validation_steps=val_steps,
-            callbacks=[early_stopping, reduce_lr],
+            callbacks=[early_stopping],
             epochs=20,
         )
 
@@ -86,13 +78,21 @@ class ActivityRecognitionModel(Sequential):
         test_dataset, test_steps = self._create_dataset(self.test_ids)
         results = self.evaluate(test_dataset, steps=test_steps, verbose=verbose)
         test_loss, test_accuracy = results[0], results[1]
-        tf.print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
+        tf.print(f"Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.4f}")
+
+    def compile_model(self):
+        optimizer = Adam(learning_rate=self.l_rate)
+        self.compile(
+            optimizer=optimizer,
+            loss="sparse_categorical_crossentropy",
+            metrics=["sparse_categorical_accuracy"],
+        )
 
     def add_input_layer(self):
         self.add(Input(shape=(self.segment_size, self.n_features)))
 
-    def add_output_layer(self, activation="softmax"):
-        self.add(Dense(self.n_classes, activation=activation))
+    def add_output_layer(self):
+        self.add(Dense(self.n_classes, activation="softmax"))
 
     def _calc_steps_per_epoch(self, session_ids):
         session_lengths = ses.fetch_all_session_lengths(session_ids)
@@ -108,57 +108,43 @@ class ActivityRecognitionModel(Sequential):
         steps_per_epoch = math.ceil(total_segments / self.batch_size)
         return steps_per_epoch
 
-    def _count_features(self):
-        columns = sen.get_sensor_columns(
-            incl_acc=self.incl_acc,
-            incl_gyro=self.incl_gyro,
-            incl_mag=self.incl_mag,
-            incl_ecg=self.incl_ecg,
-        )
-        return len(columns)
-
     def _get_shifts_arr(self):
         return [i * self.segment_size // self.n_shifts for i in range(self.n_shifts)]
 
     def _create_dataset(self, ids):
         shifts = self._get_shifts_arr()
 
-        def data_generator():
-            random.shuffle(ids)
+        def data_generator(session_id):
+            label = self.label_dict[session_id]
             with my_conn.get_db_session() as db:
-                for shift in shifts:
-                    for session_id in ids:
-                        session_data = sen.fetch_sensor_data(
-                            db,
-                            session_id,
-                            self.incl_acc,
-                            self.incl_gyro,
-                            self.incl_mag,
-                            self.incl_ecg,
-                        )
-                        label = act.fetch_activity_label(db, session_id)
-                        for offset in range(
-                            shift, len(session_data), self.segment_size
-                        ):
-                            end = offset + self.segment_size
-                            if end <= len(session_data):
-                                segment = session_data[offset:end]
-                                yield segment, label
+                session_data = sen.fetch_sensor_data(db, session_id)
+            for shift in shifts:
+                for offset in range(shift, len(session_data), self.segment_size):
+                    end = offset + self.segment_size
+                    if end <= len(session_data):
+                        segment = session_data[offset:end]
+                        yield segment, label
 
-        dataset = (
-            tf.data.Dataset.from_generator(
+        dataset = tf.data.Dataset.from_tensor_slices(ids)
+        dataset = dataset.interleave(
+            lambda x: tf.data.Dataset.from_generator(
                 data_generator,
+                args=(x,),
                 output_signature=(
                     tf.TensorSpec(
                         shape=(self.segment_size, self.n_features), dtype=tf.float32
                     ),
                     tf.TensorSpec(shape=(), dtype=tf.int32),
                 ),
-            )
-            .batch(self.batch_size)
-            .prefetch(tf.data.experimental.AUTOTUNE)
-            .repeat()
+            ),
+            cycle_length=2,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
         )
         steps_per_epoch = self._calc_steps_per_epoch(ids)
-
+        dataset = dataset.shuffle(
+            buffer_size=self.batch_size * 4, reshuffle_each_iteration=True
+        )
+        dataset = dataset.repeat()
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
         return dataset, steps_per_epoch
